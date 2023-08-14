be1390691f167f175b0d6f52fd9297f96ec43fec
Shubham Agnihotri <shubham.agnihotri1997@gmail.com>
2023-08-15 01:47:15 +0530

    Create reverse_stack.py

---
+++ DSA Supreme 1.0/11. Stacks/reverse_stack.py
@@ -0,0 +1,45 @@
+class Stack:
+    def __init__(self):
+        self.head = []
+        
+    def push(self,data):
+        self.head.append(data)
+    
+    def pop(self):
+        return self.head.pop()
+    def push_at_bottom(self,data):
+        if self.head == []:
+            self.push(data)
+            return
+            # self.head
+        temp = self.pop()
+        self.push_at_bottom(data)
+        self.push(temp)
+        
+    def print(self):
+        while self.head !=[]:
+            temp = self.pop()
+            print(temp)
+            self.push(temp)
+            
+    def reverse_stack(self):
+        if self.head ==[]:
+            return 
+        temp = self.pop()
+        self.reverse_stack()
+        self.push_at_bottom(temp)
+        
+        
+        
+        
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.push(40)
+s.push(50)
+# s.push_at_bottom(60)
+print(s.head)
+s.reverse_stack()
+print(s.head)
+# s.print()
