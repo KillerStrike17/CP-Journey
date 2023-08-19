3b3711e031a7cd9983176545a596ece996551bab
Shubham Agnihotri <shubham.agnihotri1997@gmail.com>
2023-08-19 10:48:07 +0530

    Create next smallest element in array.py

---
+++ DSA Supreme 1.0/11. Stacks/next smallest element in array.py
@@ -0,0 +1,18 @@
+
+
+v = [2,1,4,3]
+s = [-1]
+ans = []
+# for i in range(len(s)):
+#     output.append(-1)
+for i in range(len(v)-1,-1,-1):
+    curr = v[i]
+    while s[-1] >=curr and s!=[]:
+        s.pop()
+    
+    ans.append(s[-1])
+    s.append(curr)
+
+print(ans[::-1])
+    
+    
